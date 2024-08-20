d214f62b593e334e58e4ebaf19a953cf523960d5
Zhang Xiaoyu <zhangxiaoyu1017@gmail.com>
2024-08-20 20:45:25 +0800

    dev: sync edition

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,30 @@
-include!("ljx8_if_async.rs");
+include!("ljx8_if.rs");
 #[cfg(test)]
 mod tests {
     use super::*;
-    #[tokio::test]
-    async fn get_serial_number() -> Result<(), Box<dyn std::error::Error>> {
-        let mut ljx8_if_async = LJX8IFAsync::ljx8_if_ethernet_open("192.168.0.2", 24691).await?;
-        let (controller_serial, head_serial) = ljx8_if_async.ljx8_if_get_serial_number().await?;
+    #[test]
+    fn get_serial_number() -> Result<(), Box<dyn std::error::Error>> {
+        let mut ljx8_if = LJX8If::ljx8_if_ethernet_open("192.168.0.2", 24691)?;
+        let (controller_serial, head_serial) = ljx8_if.ljx8_if_get_serial_number()?;
         println!("controller_serial: {:?}", controller_serial);
         println!("head_serial: {:?}", head_serial);
         let (pn_sensor_temperature, pn_processor_temperature, pn_case_temperature) =
-            ljx8_if_async.ljx8_if_get_head_temperature().await?;
+            ljx8_if.ljx8_if_get_head_temperature()?;
         println!("pnSensorTemperature: {:?}", pn_sensor_temperature);
         println!("pnProcessorTemperature: {:?}", pn_processor_temperature);
         println!("pnCaseTemperature: {:?}", pn_case_temperature);
-        let result = ljx8_if_async.ljx8_if_communication_close().await?;
-        assert_eq!(result, 0);
+        let result = ljx8_if.ljx8_if_communication_close()?;
         Ok(())
     }
-    #[tokio::test]
-    async fn get_head_temperature() -> Result<(), Box<dyn std::error::Error>> {
-        let mut ljx8_if_async = LJX8IFAsync::ljx8_if_ethernet_open("192.168.0.2", 24691).await?;
+    #[test]
+    fn get_head_temperature() -> Result<(), Box<dyn std::error::Error>> {
+        let mut ljx8_if = LJX8If::ljx8_if_ethernet_open("192.168.0.2", 24691)?;
         let (pn_sensor_temperature, pn_processor_temperature, pn_case_temperature) =
-            ljx8_if_async.ljx8_if_get_head_temperature().await?;
+            ljx8_if.ljx8_if_get_head_temperature()?;
         println!("pnSensorTemperature: {:?}", pn_sensor_temperature);
         println!("pnProcessorTemperature: {:?}", pn_processor_temperature);
         println!("pnCaseTemperature: {:?}", pn_case_temperature);
-        let result = ljx8_if_async.ljx8_if_communication_close().await?;
-        assert_eq!(result, 0);
+        let result = ljx8_if.ljx8_if_communication_close()?;
         Ok(())
     }
 }
--- src/ljx8_if.rs
+++ src/ljx8_if.rs
@@ -1,305 +1,91 @@
-include!("ljxa_errorcode.rs");
-
-use byteorder::{ByteOrder, LittleEndian, ReadBytesExt};
-use lazy_static::lazy_static;
-use std::collections::HashMap;
-use std::io::{Error, ErrorKind, Read, Result, Write};
-use std::mem;
-use std::net::{SocketAddr, TcpStream};
-use std::sync::RwLock;
-use std::time::Duration;
-
-pub const MAX_LJXA_DEVICE_NUM: usize = 6;
-pub const MAX_LJXA_X_DATA_NUM: usize = 3200;
-pub const MAX_LJXA_Y_DATA_NUM: usize = 42000;
-
-const LJXA_CONNECT_TIMEOUT_SEC: u64 = 3;
-const WRITE_DATA_SIZE: usize = 20 * 1024;
-const SEND_BUF_SIZE: usize = WRITE_DATA_SIZE + 16;
-const RECEIVE_BUF_SIZE: usize = 3000 * 1024;
-const FAST_RECEIVE_BUF_SIZE: usize = 64 * 1024;
-const PARITY_ERROR: u32 = 0x40000000;
-
-const RES_ADRESS_OFFSET_ERROR: usize = 17;
-const PREFIX_ERROR_CONTROLLER: u32 = 0x8000;
-
-/// 设置值存储级别指定
-pub enum Ljx8ifSettingDepth {
-    Write = 0x00,   // 写入设置区域
-    Running = 0x01, // 运行设置区域
-    Save = 0x02,    // 保存区域
+use std::io::{Read, Write};
+use std::net::TcpStream;
+use std::thread::sleep;
+use std::time::{Instant, Duration};
+pub struct LJX8If {
+    stream: TcpStream,
 }
-
-/// 初始化目标设置项目指定
-pub enum Ljx8ifInitSettingTarget {
-    Prg0 = 0x00,  // 程序 0
-    Prg1 = 0x01,  // 程序 1
-    Prg2 = 0x02,  // 程序 2
-    Prg3 = 0x03,  // 程序 3
-    Prg4 = 0x04,  // 程序 4
-    Prg5 = 0x05,  // 程序 5
-    Prg6 = 0x06,  // 程序 6
-    Prg7 = 0x07,  // 程序 7
-    Prg8 = 0x08,  // 程序 8
-    Prg9 = 0x09,  // 程序 9
-    Prg10 = 0x0A, // 程序 10
-    Prg11 = 0x0B, // 程序 11
-    Prg12 = 0x0C, // 程序 12
-    Prg13 = 0x0D, // 程序 13
-    Prg14 = 0x0E, // 程序 14
-    Prg15 = 0x0F, // 程序 15
-}
-
-/// 获取配置文件目标缓冲区指定
-pub enum Ljx8ifProfileBank {
-    Active = 0x00,   // 活动表面
-    Inactive = 0x01, // 非活动表面
-}
-
-/// 获取配置文件位置规范方法指定（批量测量：关闭）
-pub enum Ljx8ifProfilePosition {
-    Current = 0x00, // 从当前
-    Oldest = 0x01,  // 从最旧
-    Spec = 0x02,    // 指定位置
-}
-
-/// 获取配置文件批次数据位置规范方法指定（批量测量：开启）
-pub enum Ljx8ifBatchPosition {
-    Current = 0x00,     // 从当前
-    Spec = 0x02,        // 指定位置
-    Commited = 0x03,    // 从批次提交后的当前
-    CurrentOnly = 0x04, // 仅当前
-}
-
-/// 版本信息结构
-pub struct Ljx8ifVersionInfo {
-    pub major_number: i32,    // 主版本号
-    pub minor_number: i32,    // 次版本号
-    pub revision_number: i32, // 修订号
-    pub build_number: i32,    // 构建号
-}
-
-/// 以太网设置结构
-pub struct Ljx8ifEthernetConfig {
-    pub ip_address: [u8; 4], // 要连接的控制器的IP地址
-    pub port_no: u16,        // 要连接的控制器的端口号
-    pub reserve: [u8; 2],    // 保留
-}
-
-/// 设置项目指定结构
-pub struct Ljx8ifTargetSetting {
-    pub type_: u8,    // 设置类型
-    pub category: u8, // 类别
-    pub item: u8,     // 设置项目
-    pub reserve: u8,  // 保留
-    pub target1: u8,  // 设置目标 1
-    pub target2: u8,  // 设置目标 2
-    pub target3: u8,  // 设置目标 3
-    pub target4: u8,  // 设置目标 4
-}
-
-/// 配置文件信息
-pub struct Ljx8ifProfileInfo {
-    pub profile_count: u8,       // 固定为1（保留供将来使用）
-    pub reserve1: u8,            // 保留
-    pub luminance_output: u8,    // 亮度输出是否开启
-    pub reserve2: u8,            // 保留
-    pub profile_data_count: u16, // 配置文件数据计数
-    pub reserve3: [u8; 2],       // 保留
-    pub x_start: i32,            // 第一点X坐标
-    pub x_pitch: i32,            // 配置文件数据X方向间隔
-}
-
-/// 配置文件头信息结构
-pub struct Ljx8ifProfileHeader {
-    pub reserve: u32,       // 保留
-    pub trigger_count: u32, // 触发发生时的触发计数
-    pub encoder_count: i32, // 触发发生时的编码器计数
-    pub reserve2: [u32; 3], // 保留
-}
-
-/// 配置文件尾信息结构
-pub struct Ljx8ifProfileFooter {
-    pub reserve: u32, // 保留
-}
-
-/// 获取配置文件请求结构（批量测量：关闭）
-pub struct Ljx8ifGetProfileRequest {
-    pub target_bank: u8,       // 要读取的目标表面
-    pub position_mode: u8,     // 获取配置文件位置规范方法
-    pub reserve: [u8; 2],      // 保留
-    pub get_profile_no: u32,   // 要获取的配置文件的配置文件号
-    pub get_profile_count: u8, // 要读取的配置文件数量
-    pub erase: u8,             // 指定是否擦除已读取的配置文件数据和比其更旧的配置文件数据
-    pub reserve2: [u8; 2],     // 保留
-}
-
-/// 获取配置文件请求结构（批量测量：开启）
-pub struct Ljx8ifGetBatchProfileRequest {
-    pub target_bank: u8,       // 要读取的目标表面
-    pub position_mode: u8,     // 获取配置文件位置规范方法
-    pub reserve: [u8; 2],      // 保留
-    pub get_batch_no: u32,     // 要获取的配置文件的批次号
-    pub get_profile_no: u32,   // 在指定批次号中开始获取配置文件的配置文件号
-    pub get_profile_count: u8, // 要读取的配置文件数量
-    pub erase: u8,             // 指定是否擦除已读取的配置文件数据和比其更旧的配置文件数据
-    pub reserve2: [u8; 2],     // 保留
-}
-
-/// 获取配置文件响应结构（批量测量：关闭）
-pub struct Ljx8ifGetProfileResponse {
-    pub current_profile_no: u32, // 当前时间点的配置文件号
-    pub oldest_profile_no: u32,  // 控制器持有的最旧配置文件的配置文件号
-    pub get_top_profile_no: u32, // 这次读取的最旧配置文件的配置文件号
-    pub get_profile_count: u8,   // 这次读取的配置文件数量
-    pub reserve: [u8; 3],        // 保留
-}
-
-/// 获取配置文件响应结构（批量测量：开启）
-pub struct Ljx8ifGetBatchProfileResponse {
-    pub current_batch_no: u32,            // 当前时间点的批次号
-    pub current_batch_profile_count: u32, // 最新批次中的配置文件数量
-    pub oldest_batch_no: u32,             // 控制器持有的最旧批次的批次号
-    pub oldest_batch_profile_count: u32,  // 控制器持有的最旧批次中的配置文件数量
-    pub get_batch_no: u32,                // 这次读取的批次号
-    pub get_batch_profile_count: u32,     // 这次读取的批次中的配置文件数量
-    pub get_batch_top_profile_no: u32,    // 这次读取的批次中最旧的配置文件号
-    pub get_profile_count: u8,            // 这次读取的配置文件数量
-    pub current_batch_commited: u8,       // 最新批次号的批量测量已完成
-    pub reserve: [u8; 2],                 // 保留
-}
-
-/// 高速通信准备开始请求结构
-pub struct Ljx8ifHighSpeedPreStartReq {
-    pub send_position: u8, // 发送开始位置
-    pub reserve: [u8; 3],  // 保留
-}
-
-lazy_static! {
-    static ref DEVICE_STREAMS: RwLock<HashMap<i32, TcpStream>> = RwLock::new(HashMap::new());
-}
-pub fn ljx8_if_initialize() -> u32 {
-    LJX8IF_RC_OK
-}
-pub fn ljx8_if_finalize() -> u32 {
-    LJX8IF_RC_OK
-}
-pub fn ljx8_if_get_version() -> Ljx8ifVersionInfo {
-    Ljx8ifVersionInfo {
-        major_number: 1,
-        minor_number: 0,
-        revision_number: 0,
-        build_number: 0,
+impl LJX8If {
+    pub fn ljx8_if_ethernet_open(ip: &str, port: u16) -> Result<Self, std::io::Error> {
+        let stream = TcpStream::connect((ip, port))?;
+        Ok(Self { stream })
     }
-}
-pub fn ljx8_if_ethernet_open(l_device_id: i32, p_ethernet_config: &Ljx8ifEthernetConfig) -> u32 {
-    // 检查输入参数
-    if p_ethernet_config.ip_address.len() != 4 {
-        return LJX8IF_RC_ERR_PARAMETER;
+    pub fn ljx8_if_communication_close(&mut self) -> Result<(), std::io::Error> {
+        self.stream.shutdown(std::net::Shutdown::Both)?;
+        Ok(())
     }
-
-    // 构造IP地址字符串
-    let ip = format!(
-        "{}.{}.{}.{}",
-        p_ethernet_config.ip_address[0],
-        p_ethernet_config.ip_address[1],
-        p_ethernet_config.ip_address[2],
-        p_ethernet_config.ip_address[3]
-    );
-
-    // 构造SocketAddr
-    let addr = SocketAddr::new(ip.parse().unwrap(), p_ethernet_config.port_no);
-
-    // 尝试连接，设置超时
-    match TcpStream::connect_timeout(&addr, Duration::from_secs(LJXA_CONNECT_TIMEOUT_SEC)) {
-        Ok(stream) => {
-            // 将stream存储在一个全局的HashMap中，以l_device_id为键
-            DEVICE_STREAMS.write().unwrap().insert(l_device_id, stream);
-            LJX8IF_RC_OK
-        }
-        Err(e) => match e.kind() {
-            ErrorKind::TimedOut => LJX8IF_RC_ERR_OPEN,
-            _ => LJX8IF_RC_ERR_OPEN,
-        },
+    pub fn ljx8_if_get_head_temperature(&mut self) -> Result<(i16, i16, i16), std::io::Error> {
+        let receive_buffer = self.send_single_command(0x4C)?;
+        let pn_sensor_temperature = i16::from_le_bytes(receive_buffer[28..30].try_into().unwrap());
+        let pn_processor_temperature =
+            i16::from_le_bytes(receive_buffer[30..32].try_into().unwrap());
+        let pn_case_temperature = i16::from_le_bytes(receive_buffer[32..34].try_into().unwrap());
+        Ok((
+            pn_sensor_temperature,
+            pn_processor_temperature,
+            pn_case_temperature,
+        ))
     }
-}
-pub fn ljx8_if_communication_close(l_device_id: i32) -> u32 {
-    let mut streams = DEVICE_STREAMS.write().unwrap();
-    if let Some(stream) = streams.remove(&l_device_id) {
-        stream.shutdown(std::net::Shutdown::Both).unwrap();
-    }
-    LJX8IF_RC_OK
-}
+    pub fn ljx8_if_get_serial_number(&mut self) -> Result<(String, String), std::io::Error> {
+        let receive_buffer = self.send_single_command(0x06)?;
+        let number_of_unit = receive_buffer[28];
+        let mut controller_serial = String::new();
+        let mut head_serial = String::new();
 
-fn my_any_command(l_device_id: i32, send_data: &[u8], data_length: i32) -> u32 {
-    let data_offset = 16;
-    let mut send_buffer = [0; SEND_BUF_SIZE];
-
-    let buf_slice = send_buffer.as_mut_slice();
-    buf_slice[4] = 0x03;
-    buf_slice[5] = 0x00;
-    buf_slice[6] = 0xF0;
-    buf_slice[7] = 0x00;
-
-    let length_bytes = (data_length as u32).to_le_bytes();
-    buf_slice[12..16].copy_from_slice(&length_bytes);
-
-    buf_slice[data_offset..data_offset + data_length as usize].copy_from_slice(send_data);
-    let tcp_length = data_offset + data_length as usize - 4;
-    LittleEndian::write_u32(&mut send_buffer[0..4], tcp_length as u32);
-
-    let streams = DEVICE_STREAMS.read().unwrap();
-    if let Some(mut stream) = streams.get(&l_device_id).and_then(|s| s.try_clone().ok()) {
-        match stream.write_all(&send_buffer[..tcp_length + 4]) {
-            Ok(_) => {
-                // 发送成功后，尝试查看接收的数据
-                let mut peek_buffer = [0u8; RECEIVE_BUF_SIZE];
-                let mut n = 0;
-                while n < 4 {
-                    match stream.peek(&mut peek_buffer[..]) {
-                        Ok(m) => n = m,
-                        Err(_) => return LJX8IF_RC_ERR_RECEIVE,
-                    }
-                }
-                let mut receive_buffer = vec![0u8; RECEIVE_BUF_SIZE];
-                // 读取前4个字节以获取总长度
-                let _ = stream.read_exact(&mut receive_buffer[..4]);
-                let recv_length =
-                    (&receive_buffer[..4]).read_u32::<LittleEndian>().unwrap() as usize;
-                let target_length = recv_length + 4;
-                // 确保buffer大小足够
-                if receive_buffer.len() < target_length {
-                    receive_buffer.resize(target_length, 0);
-                }
-                let mut received_bytes = 4;
-                while received_bytes < target_length {
-                    let try_bytes = std::cmp::min(target_length - received_bytes, 65535);
-
-                    // 使用peek来检查可用数据
-                    let mut n = 0;
-                    while n < try_bytes {
-                        n = stream
-                            .peek(&mut receive_buffer[received_bytes..received_bytes + try_bytes])
-                            .unwrap();
-                    }
-
-                    // 实际读取数据
-                    stream
-                        .read_exact(&mut receive_buffer[received_bytes..received_bytes + try_bytes])
-                        .unwrap();
-                    received_bytes += try_bytes;
-                }
-                receive_buffer.truncate(target_length);
-                let err_code = receive_buffer[RES_ADRESS_OFFSET_ERROR];
-
-                if err_code != 0 {
-                    return PREFIX_ERROR_CONTROLLER + err_code as u32;
-                }
-                LJX8IF_RC_OK
+        if number_of_unit >= 1 {
+            controller_serial = std::str::from_utf8(&receive_buffer[76..92])
+                .unwrap()
+                .trim_end_matches('\0')
+                .to_string();
+            if number_of_unit >= 2 {
+                head_serial = std::str::from_utf8(&receive_buffer[128..144])
+                    .unwrap()
+                    .trim_end_matches('\0')
+                    .to_string();
             }
-            Err(_) => LJX8IF_RC_ERR_SEND,
         }
-    } else {
-        LJX8IF_RC_ERR_NOT_OPEN
+
+        Ok((controller_serial, head_serial))
+    }
+    fn send_single_command(&mut self, code: u8) -> Result<Vec<u8>, std::io::Error> {
+        let command = [code, 0x00, 0x00, 0x00];
+        self.my_any_command(&command)
+    }
+    fn my_any_command(&mut self, command: &[u8]) -> Result<Vec<u8>, std::io::Error> {
+        let mut send_buffer = vec![0; 20 * 1024 + 16];
+        let data_offset = 16;
+        let tcp_length = data_offset + command.len() as u32 - 4;
+        let tcp_length_bytes = tcp_length.to_le_bytes();
+        send_buffer[..4].copy_from_slice(&tcp_length_bytes);
+        send_buffer[4] = 0x03;
+        send_buffer[5] = 0x00;
+        send_buffer[6] = 0xF0;
+        send_buffer[7] = 0x00;
+        let length_bytes = (command.len() as u32).to_le_bytes();
+        send_buffer[12..16].copy_from_slice(&length_bytes);
+        send_buffer[data_offset as usize..data_offset as usize + command.len()]
+            .copy_from_slice(command);
+        send_buffer.truncate((tcp_length + 4) as usize);
+        println!("send_buffer: {:02X?}", send_buffer);
+        self.stream.write_all(&send_buffer)?;
+        let mut length_buffer = vec![0u8; 4];
+        let timeout = Duration::from_secs(5); // 设置5秒超时
+        let start_time = Instant::now();
+
+        while self.stream.peek(&mut length_buffer)? < 4 {
+            if start_time.elapsed() > timeout {
+                return Err(std::io::Error::new(std::io::ErrorKind::TimedOut, "Timeout"));
+            }
+            sleep(Duration::from_millis(100));
+        }
+
+        let recv_length = u32::from_le_bytes(length_buffer[..].try_into().unwrap());
+        let target_length = recv_length + 4;
+        let mut receive_buffer = vec![0u8; target_length as usize];
+        let _ = self.stream.read_exact(&mut receive_buffer)?;
+
+        println!("receive_buffer: {:02X?}", receive_buffer);
+        println!("target_length: {:?}", target_length);
+        Ok(receive_buffer)
     }
 }
